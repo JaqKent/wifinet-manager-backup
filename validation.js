14a449c682f05bf05d1635a910a6815e70a88f49
Luis Ramirez <jaq.kent@gmail.com>
2024-02-14 10:42:01 -0300

    adding players to the database

--- validation.js
+++ validation.js
@@ -33,7 +33,6 @@ const clientValidation = (data) => {
     inscriptionDate: Joi.date().optional().empty(""),
     unSubscribingDate: Joi.date().optional().empty(""),
     unSubscribingReason: Joi.string().trim().optional().empty(""),
-    plan: Joi.string().required().trim(),
     price: Joi.number().required(),
     priceText: Joi.string().required().trim(),
     priceInstall: Joi.number().optional().empty(""),
@@ -68,7 +67,58 @@ const billValidation = (data) => {
   return schema.validate(data);
 };
 
+const playersValidation = (data) => {
+  const schema = Joi.object({
+    name: Joi.string().required().trim(),
+    address: Joi.string().required().trim(),
+    dni: Joi.string().required().trim(),
+    birtday: Joi.date().required(),
+    category: dni: Joi.string().optional().trim().empty(""),
+    inscriptionDate: Joi.date().optional().empty(""),
+    unSubscribingDate: Joi.date().optional().empty(""),
+    unSubscribingReason: Joi.string().trim().optional().empty(""),
+    price: Joi.number().required(),
+    priceText: Joi.string().required().trim(),
+    priceInstall: Joi.number().optional().empty(""),
+    phone: Joi.number().required(),
+    phoneAlt: Joi.number().optional().empty(""),
+    obraSocial: Joi.string().optional().trim().empty("")
+    nickName: Joi.string().optional().trim().empty("")
+    bloodType: Joi.string().optional().trim().empty("")
+    tshirtSize: Joi.string().optional().trim().empty("")
+    shortSize: Joi.string().optional().trim().empty("")
+    email: Joi.string().email().optional().empty(""),
+    ipAddress: Joi.string().ip().optional().empty(""),
+    isDown: Joi.boolean().required(),
+    isSaving: Joi.boolean(),
+    createdBy: Joi.string().trim(),
+  });
+  return schema.validate(data);
+};
+
+const playerBillsValidation = (data) => {
+  const schema = Joi.object({
+    billNumber: Joi.number().required(),
+    createdAt: Joi.date().required(),
+    dueDate: Joi.date().required(),
+    playerInfo: {
+      id: Joi.string().trim(),
+      name: Joi.string().trim(),
+      address: Joi.string().trim(),
+      dni: Joi.string().required().trim(),
+    },
+    price: Joi.number().required(),
+    priceText: Joi.string().trim().required(),
+    month: Joi.string().trim().required(),
+    additionalNotes: Joi.string(),
+  });
+
+  return schema.validate(data);
+};
+
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
 module.exports.clientValidation = clientValidation;
 module.exports.billValidation = billValidation;
+module.exports.playersValidation = playersValidation;
+module.exports.playerBillsValidation = playerBillsValidation;
