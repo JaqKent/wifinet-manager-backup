156c16e852dc83d276639d5fec21621684638995
Luis Ramirez <jaq.kent@gmail.com>
2024-04-05 22:06:21 -0300

    userBills

--- routes/playerBills.js
+++ routes/playerBills.js
@@ -42,7 +42,7 @@ router.get("/get", auth, (req, res) => {
         .catch((err) => res.send({ success: false, message: err.message }));
 });
 
-router.get(`/get/:id`, auth, (req, res) => {
+router.get(`/get/:id`, (req, res) => {
     PlayersBill.find({ _id: req.params.id })
         .then((i) => res.send({ success: true, data: i }))
         .catch((err) => res.send({ success: false, message: err.message }));
